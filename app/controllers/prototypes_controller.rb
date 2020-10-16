38d4296a67b34ec5201a7e65184695f6156dcf0d
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-10-16 14:18:41 +0900

    投稿内容編集ページ/削除機能

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -20,6 +20,29 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.find(params[:id])
   end
 
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    if prototype.update(proto_params)
+      redirect_to prototype_path
+    else
+      @prototype = Prototype.find(params[:id])
+      @prototype.title = params[:prototype][:title]
+      @prototype.catch_copy = params[:prototype][:catch_copy]
+      @prototype.concept = params[:prototype][:concept]
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
   private
   def proto_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
