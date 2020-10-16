6646d715133e1018687bc18fd944c1c4bfee231c
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-10-16 17:17:07 +0900

    ユーザーのページ繊維権限の設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,7 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except:[:index, :show]
+  before_action :valid_user?, only:[:edit]
+
   def index
     @prototypes = Prototype.all.order("created_at DESC")
   end
@@ -50,4 +53,10 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
+  def valid_user?
+    @prototype = Prototype.find(params[:id])
+    unless current_user.id == @prototype.user_id
+      redirect_to root_path
+    end
+  end
 end
